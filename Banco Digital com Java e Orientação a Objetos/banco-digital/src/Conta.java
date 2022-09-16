840265183f208b2a1fac1511e300f1a05548ae78
luispaulobrito <luiss_paullo@hotmail.com>
2022-09-16 11:46:08 -0300

    polimorfismo

--- Banco Digital com Java e Orientação a Objetos/banco-digital/src/Conta.java
+++ Banco Digital com Java e Orientação a Objetos/banco-digital/src/Conta.java
@@ -1,8 +1,14 @@
 public abstract class Conta implements IConta{
-    public int agencia;
-    public int numero;
-    public double saldo;
+    private static final int AGENCIA_PADRAO = 1;
+    private static int SEQUENCIAL = 1;
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
 
+    public Conta() {
+        this.agencia = AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
     public int getAgencia() {
         return agencia;
     }
